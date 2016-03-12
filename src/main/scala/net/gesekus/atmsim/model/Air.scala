77cefb9cf483d502b974d7f9b77fee74a33ca5ee
timgesekus <tim@gesekus.net>
2016-03-12 09:25:13 +0100

    Initial import

---
+++ src/main/scala/net/gesekus/atmsim/model/Air.scala
@@ -0,0 +1,7 @@
+package net.gesekus.atmsim.model
+
+case class Degree(angel: Double)
+case class Position3D(x: Double, y: Double, z: Double)
+case class AircraftId(id: Int)
+
+case class Aircraft(id: AircraftId, heading: Degree, position: Position3D)
